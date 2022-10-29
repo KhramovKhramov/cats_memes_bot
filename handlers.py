c78cbc41395a1db21554369b46dd377e89e10312
Pavel Khramov <paul.khramov@gmail.com>
2022-10-29 15:17:57 +0600

    feature: /start handler added

--- handlers.py
+++ handlers.py
@@ -1,2 +1,8 @@
+from db import db, get_or_create_user
+
+
 def greet_user(update, context):
-    pass
+    user = get_or_create_user(
+        db, update.effective_user,
+        update.message.chat.id)
+    update.message.reply_text(f'Привет, котик {user["first_name"]}!')
