81f740ae5cfc6a520c55235e1a9169cfa9d4a3ec
Pavel Khramov <paul.khramov@gmail.com>
2022-10-29 19:49:57 +0600

    feature: 'get pic' and 'send pic' handlers added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import logging
 
-from telegram.ext import CommandHandler, Updater
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 import settings
-from handlers import greet_user
+from handlers import send_pic, greet_user, get_pic, want_to_send_pic
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
@@ -13,6 +13,13 @@ def main():
     dp = mybot.dispatcher
 
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.regex(
+        '^(Отправить мемас)$'),
+        want_to_send_pic))
+    dp.add_handler(MessageHandler(Filters.photo, get_pic))
+    dp.add_handler(MessageHandler(Filters.regex(
+        '^(Получить мемас)$'),
+        send_pic))
 
     logging.info('bot has started')
     mybot.start_polling()
